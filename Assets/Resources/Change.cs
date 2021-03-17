d55335319223eebbe8f7b5db5f334175c1f83413
Jerrin Thomas <jd0christo1678@gmail.com>
2021-03-17 12:48:03 +0530

    Added Unity Files

---
+++ Assets/Resources/Change.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change : MonoBehaviour
+{
+    public GameObject gs;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveRight()
+    {
+        anim.Play("Walking");
+    }
+    public void MoveLeft()
+    {
+        anim.Play("Happy Idle");
+    }
+    public void Stunt()
+    {
+        anim.Play("Run To Flip");
+    }
+}
